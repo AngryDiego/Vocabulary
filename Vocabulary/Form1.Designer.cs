95513c90abb51dbb7be835d9c1427bb5a0b4d87d
DiegoVS <Diegoanchukov@gmail.com>
2025-09-21 15:57:05 +0200

    Add project files.

---
+++ Vocabulary/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Vocabulary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.txtMeaning = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.txtSearch);
+            this.panel1.Controls.Add(this.btnLoad);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.txtMeaning);
+            this.panel1.Controls.Add(this.txtWord);
+            this.panel1.Location = new System.Drawing.Point(66, 47);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(616, 361);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtWord
+            // 
+            this.txtWord.Location = new System.Drawing.Point(59, 47);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(151, 22);
+            this.txtWord.TabIndex = 0;
+            // 
+            // txtMeaning
+            // 
+            this.txtMeaning.Location = new System.Drawing.Point(233, 47);
+            this.txtMeaning.Name = "txtMeaning";
+            this.txtMeaning.Size = new System.Drawing.Size(254, 22);
+            this.txtMeaning.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(233, 75);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(233, 122);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(254, 180);
+            this.listBox1.TabIndex = 3;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(516, 166);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(516, 195);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(516, 224);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 6;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(233, 311);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(144, 22);
+            this.txtSearch.TabIndex = 7;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(383, 311);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtMeaning;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ Vocabulary/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vocabulary
+{
+    public partial class Form1: Form
+    {
+        private Dictionary<string, string> internetSlangDict = new Dictionary<string, string>();
+        private string filePath = "slang.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // dictionary[Key] = Value
+
+            string word = txtWord.Text.Trim();
+            string meaning = txtMeaning.Text;
+
+            if (!string.IsNullOrEmpty(word) && !string.IsNullOrEmpty(meaning))
+            {
+                internetSlangDict[word] = meaning; // o kak
+                txtWord.Clear();
+                txtMeaning.Clear();
+                UpdateList();
+            }
+            else
+            {
+                MessageBox.Show("Ti debil?");
+            }
+        }
+
+        private void UpdateList()
+        {
+            listBox1.Items.Clear();
+            foreach(var pair in internetSlangDict)
+            {
+                // метод інтерполяції
+                listBox1.Items.Add($"{pair.Key} - {pair.Value}");// (KeyEx) - (ValueEx)
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedItem != null)
+            {                                                 //(сепаратор)[ітератор листа].Trim
+                string word = listBox1.SelectedItem.ToString().Split('-')[0].Trim();//list ["KeyEx "," ValueEx"]
+                internetSlangDict.Remove(word); // if(dict.Remove(word)
+                UpdateList();
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string search = txtSearch.Text.Trim();
+            
+            if (search != "")
+            {
+                listBox1.Items.Clear();
+
+                foreach (var pair in internetSlangDict)
+                {
+                    if (pair.Key.StartsWith(search))
+                    {
+                        listBox1.Items.Add($"{pair.Key} - {pair.Value}");
+                    }
+                }
+                if(listBox1.Items.Count == 0)
+                {
+                    MessageBox.Show("404 not found");
+                    UpdateList();
+                }
+            }
+            else if (search == "")
+            {
+                UpdateList();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                foreach(var pair in internetSlangDict)
+                {
+                    // cout << stroing;
+                    // file << string
+                    // Console.WriteLine == cout 
+                    sw.WriteLine($"{pair.Key}:{pair.Value}");  // "wearher":"sunny"
+                }
+            } // 800kb
+
+            //600kb
+            MessageBox.Show("Dictionary saved");
+
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(filePath))
+            {
+                internetSlangDict.Clear();
+
+                foreach (var line in File.ReadAllLines(filePath))
+                {
+                    var parts = line.Split(':'); 
+                    if(parts.Length == 2)
+                    {
+                        internetSlangDict[parts[0]] = parts[1];  // додаєш або оновлюєш існуючий
+                    }
+                }
+                UpdateList();
+
+                MessageBox.Show("List has been loaded");
+            }
+        }
+    }
+}
